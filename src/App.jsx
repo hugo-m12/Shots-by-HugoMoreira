e299888fe8c0e9b625d912ed89cac54221c78d8e
Hugo Moreira <hugo.m@live.com.pt>
2026-03-10 19:55:52 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,20 @@ register();
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <Switch>
-          <Route exact path="/" component={HomeView} />
-          <Route path="/About-me" component={AboutView} />
-          <Route path="/Contact" component={ContactView} />
-          <Route path="/404" component={Error404View} />
-          <Route path="/AddLocation" component={AddLocationView} />
-          <Route path="/:locationName" component={LocationDetailView} />
-        </Switch>
-      </main>
-      <Footer />
-    </div>
+    <>
+    <Header />
+
+      <Switch>
+        <Route exact path="/" component={HomeView} />
+        <Route path="/About-me" component={AboutView} />
+        <Route path="/Contact" component={ContactView} />
+        <Route path="/404" component={Error404View} />
+        <Route path="/AddLocation" component={AddLocationView} />
+        <Route path="/:locationName" component={LocationDetailView} />
+      </Switch>
+      
+    <Footer />
+    </>
   );
 }
 
