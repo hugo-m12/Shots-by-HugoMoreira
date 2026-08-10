b4948217f168a278b82f61496fbd81cc65f451af
Hugo Moreira <hugo.m@live.com.pt>
2026-08-10 14:19:31 +0100

    fix: onclick prop locations on globe

--- src/components/LocationsGlobe.jsx
+++ src/components/LocationsGlobe.jsx
@@ -1,6 +1,8 @@
 import Globe from "react-globe.gl";
+import { useLocation } from "wouter";
 
 function LocationsGlobe() {
+  const [, setLocation] = useLocation();
 
   const pointsData = [
     {
@@ -8,48 +10,56 @@ function LocationsGlobe() {
       lng: -8.62,
       color: "black",
       label: "Porto",
+      slug: "porto",
     },
     {
       lat: 41.38,
       lng: 2.13,
       color: "orange",
       label: "Barcelona",
+      slug: "barcelona",
     },
     {
       lat: 42.43,
       lng: -8.70,
       color: "blue",
       label: "Combarro",
+      slug: "combarro",
     },
     {
       lat: 52.37,
       lng: 4.89,
       color: "red",
       label: "Amsterdam",
+      slug: "amsterdam",
     },
     {
       lat: 38.79,
       lng: -9.38,
       color: "yellow",
       label: "Sintra",
+      slug: "sintra",
     },
     {
       lat: 52.50,
       lng: 13.33,
       color: "white",
       label: "Berlin",
+      slug: "berlin",
     },
     {
       lat: 40.41,
       lng: -3.70,
       color: "purple",
       label: "Madrid",
+      slug: "madrid",
     },
     {
       lat: 53.35,
       lng: -6.26,
       color: "green",
       label: "Dublin",
+      slug: "dublin",
     },
   ];
   
@@ -71,12 +81,9 @@ function LocationsGlobe() {
         autoRotate={true}
         autoRotateSpeed={3}
         enablePointerInteraction={true}
-        onPointClick={(point) => {
-    console.log(point.label);
-  }}
+        onPointClick={(point) => setLocation(`/${point.slug}`)}
       />
     </div>
   );
 }
-
 export default LocationsGlobe;
