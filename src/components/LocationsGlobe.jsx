7689d86fd602e3bb04c031dae70072adf90468a1
Hugo Moreira <hugo.m@live.com.pt>
2026-08-10 12:23:25 +0100

    fix: cursor pointer on globe

--- src/components/LocationsGlobe.jsx
+++ src/components/LocationsGlobe.jsx
@@ -1,6 +1,7 @@
 import Globe from "react-globe.gl";
 
 function LocationsGlobe() {
+
   const pointsData = [
     {
       lat: 41.15,
@@ -47,23 +48,25 @@ function LocationsGlobe() {
   ];
   
   return (
-    <Globe
-      backgroundColor="rgba(255,255,255,255)"
-      labelText={"label"}
-      labelSize={1.6}
-      width={400}
-      height={600}
-      hexPolygonResolution={3}
-      pointsData={pointsData}
-      pointLabel="label"
-      pointColor="color"
-      markerRadius={0.5}
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
-      bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-      autoRotate={true}
-      autoRotateSpeed={3}
-      enablePointerInteraction={true}
-    />
+    <div className="cursor-pointer">
+      <Globe
+        backgroundColor="rgba(255,255,255,255)"
+        labelText={"label"}
+        labelSize={1.6}
+        width={400}
+        height={600}
+        hexPolygonResolution={3}
+        pointsData={pointsData}
+        pointLabel="label"
+        pointColor="color"
+        markerRadius={0.5}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+        autoRotate={true}
+        autoRotateSpeed={3}
+        enablePointerInteraction={true}
+      />
+    </div>
   );
 }
 
